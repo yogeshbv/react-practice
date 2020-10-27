60c3e75c65c6984c6f8532db8b1aae2edf1a2806
yogeshbv <yogeshbv09@gmail.com>
2020-10-27 16:42:42 +0530

    Updates

---
+++ precache-manifest.fa0ef7ae4d5127447b14cf77f07065d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563cac6d4bb35d1eb677bb475f09f9aa",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "50b820b0dd94781b736a",
+    "url": "/react-practice/static/css/main.feddad60.chunk.css"
+  },
+  {
+    "revision": "78b2c0d0f6dc2f40f8d5",
+    "url": "/react-practice/static/js/2.8163db98.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-practice/static/js/2.8163db98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b820b0dd94781b736a",
+    "url": "/react-practice/static/js/main.89220fe0.chunk.js"
+  },
+  {
+    "revision": "b646f1b52b3f442cbc64",
+    "url": "/react-practice/static/js/runtime-main.c53e414e.js"
+  }
+]);
